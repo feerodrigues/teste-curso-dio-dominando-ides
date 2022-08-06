3ec160f0f3aca409a4e3735004f66ea2d1613fc7
Fernanda Rodrigues <fe.fe14@hotmail.com>
2022-08-06 00:42:52 -0300

    First Commit

---
+++ src/br/com/dio/Primeiroprograma.java
@@ -0,0 +1,16 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class Primeiroprograma {
+
+	public static void main(String[] args) {
+
+		Gato gato = new Gato(null, null, null);
+				System.out.println(gato);
+		
+		
+
+	}
+
+}
